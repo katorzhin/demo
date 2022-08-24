ebc976728d3797ada6e347ed2b2a4b29901deff8
Katorzhin <alex.katorzhin@gmail.com>
2022-08-25 01:24:48 +0300

    added request insert into table

---
+++ src/main/resources/db.migration/V002.02__insert_cities.sql
@@ -0,0 +1,10 @@
+insert into cities(country_id, name)
+values (1, 'Kiev'),
+       (1, 'Kharkiv'),
+       (1, 'Sevastopol'),
+       (2, 'Telaviv'),
+       (2, 'Haifa'),
+       (2, 'Hdot'),
+       (3, 'Kishinev'),
+       (3, 'Tiraspol'),
+       (3, 'Beer Sheva');
